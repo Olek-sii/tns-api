183faa51b2fa3e831ace05709467628f58213044
olek-sii <oleksii.al@gmail.com>
2017-12-14 00:06:40 +0100

    refresh_token update fix [2]

--- app/controllers/google_api_controller.rb
+++ app/controllers/google_api_controller.rb
@@ -19,12 +19,12 @@ class GoogleApiController < ApplicationController
     google_tokens = JSON.parse(current_user.google_tokens)
     if google_tokens['expires_at'] < Time.now.to_i
       data = {
-          :client_id => Rails.application.secrets.GOOGLE_CLIENT_ID,
-          :client_secret => Rails.application.secrets.GOOGLE_SECRET,
-          :refresh_token => google_tokens['refresh_token'],
-          :grant_type => 'refresh_token'
+        :client_id => Rails.application.secrets.GOOGLE_CLIENT_ID,
+        :client_secret => Rails.application.secrets.GOOGLE_SECRET,
+        :refresh_token => google_tokens['refresh_token'],
+        :grant_type => 'refresh_token'
       }
-      response = JSON.decode(RestClient.post 'https://accounts.google.com/o/oauth2/token', data)
+      response = ActiveSupport::JSON.decode(RestClient.post 'https://accounts.google.com/o/oauth2/token', data)
       google_tokens['token'] = response['access_token']
       google_tokens['expires_at'] = response['expires_in'] + Time.now.to_i
       current_user.google_tokens = google_tokens.to_json
